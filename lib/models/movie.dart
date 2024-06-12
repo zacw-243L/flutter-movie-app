14f55a56ab7b6944f8552f49c8c8a3d27d9beb32
zacw-243L <qwerty.amousy123442x@gmail.com>
2024-06-12 15:24:09 +0800

    Start

---
+++ lib/models/movie.dart
@@ -0,0 +1,14 @@
+class Movie {
+  final String title;
+  final String caption;
+  final String url;
+
+  Movie({
+    required this.title,
+    required this.caption,
+    required this.url,
+  });
+
+  //TODO implement Movie.fromJson
+
+}
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String strYear = '2023';
+
+  // TODO Implement fetchMovies async function that takes in a parameter (year), makes an API request, processes response and returns List<Movie>
+  fetchMovies() async {
+    String baseURL = '';
+
+    Map<String, String> requestHeaders = {};
+
+    Map<String, String> queryParams = {
+      //TODO Add query parameters
+      'list': 'top_boxoffice_200',
+    };
+
+    //DO NOT EDIT
+    String queryString = Uri(queryParameters: queryParams).query;
+    final response = await http.get(
+      Uri.parse(baseURL + '?' + queryString),
+      headers: requestHeaders,
+    );
+
+    if (response.statusCode == 200) {
+      // TODO
+    } else {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: Container(),
+      //TODO Implement FloatingActionButton to navigate to SearchScreen
+
+      appBar: AppBar(
+        title: const Text('Movies'),
+      ),
+
+      body: Container(), //TODO Implement FutureBuilder
+    );
+  }
+}
