3a89fd70b443a7cad50cb7002946d710fd856285
zacw-243L <58255472+zacw-243L@users.noreply.github.com>
2024-06-12 16:13:47 +0800

    Update movie.dart

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -12,12 +12,12 @@ class Movie {
   //TODO implement Movie.fromJson
   factory Movie.fromJson(Map<String, dynamic> json) {
     return Movie(
-      title: json['titleText']['text'],
-      caption: json['primaryImage']['caption']['plainText'],
-      url: json['primaryImage']['url'],
-      // title: json['results'][0]['titleText']['text'],
-      // caption: json['results'][0]['primaryImage']['caption']['plainText'],
-      // url: json['results'][0]['primaryImage']['url'],
+      // title: json['titleText']['text'],
+      // caption: json['primaryImage']['caption']['plainText'],
+      // url: json['primaryImage']['url'],
+      title: json['results'][0]['titleText']['text'],
+      caption: json['results'][0]['primaryImage']['caption']['plainText'],
+      url: json['results'][0]['primaryImage']['url'],
     );
   }
 }
